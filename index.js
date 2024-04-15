64cf19e58844fe931d8723dea2cf96d943302291
my <116833855+43984927@users.noreply.github.com>
2024-04-15 09:46:07 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-console.log("Hello Github Pages");
+window.onload = function() {
+    var timestampDiv = document.getElementById('timestamp');
+    var d = new Date();
+    var timestamp = d.toLocaleString(); // Get the current timestamp in a human-readable format
+    timestampDiv.innerHTML = "File generated at: " + timestamp;
+};
